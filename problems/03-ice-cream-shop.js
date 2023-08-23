5caad47c5772ec064ffca6534ea93318839d3d00
Yasuhiro Sekiyama <sekiyamayasuhiro@gmail.com>
2023-08-22 21:33:15 -0700

    problem03

--- problems/03-ice-cream-shop.js
+++ problems/03-ice-cream-shop.js
@@ -6,7 +6,6 @@ offers their favorite flavor.
 
 Examples:
 iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
-iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
 iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
 iceCreamShop(['moose tracks'], 'moose tracks'); // true
 iceCreamShop([], 'honey lavender'); // false
@@ -14,9 +13,20 @@ iceCreamShop([], 'honey lavender'); // false
 
 
 function iceCreamShop(flavors, favorite) {
-  // Your code here 
+  // Your code here
+  if (flavors.length === 0) {
+    return false;
+  };
+
+  if (flavors[0] === favorite) {
+    return true;
+  }
+
+  return iceCreamShop(flavors.slice(1), favorite);
+
 }
 
+iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
